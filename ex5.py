321615a305ac9bbe066810a0ae17f80511990b61
Nabilus <nabil.djebbar@etu.univ-lyon1.fr>
2020-10-27 15:49:24 +0100

    SVMs Ensemble performance

--- ex5.py
+++ ex5.py
@@ -2,6 +2,7 @@ from sklearn import datasets
 import numpy as np
 from sklearn import svm
 import random
+from matplotlib import pyplot as plt
 
 
 def SVM_Enssemble(nb_classifier,x_train,y_train,percentage):
@@ -27,26 +28,39 @@ def SVM_Enssemble(nb_classifier,x_train,y_train,percentage):
 	return models
 
 
+def prediction(x,models,nb_classes):
+
+	cl = np.zeros(nb_classes)
+	for i in range(len(models)):
+		cl[models[i].predict([x])] = cl[models[i].predict([x])] + 1
+
+	return np.argmax(cl)
+
+
+def prdict_test(x_test,models,nb_classes):
+
+	y_test = []
+	for i in range(len(x_test)):
+		y_test.append(prediction(x_test[i],models,nb_classes))
+
+	return y_test
+
 #Perofrmance test  
 
 
 # rapport entre le nombre de hits sur le totale 
 
 
-det Perofrmance_model(x_test,y_test,model):
-	percentage = 0
-	return percentage
-
 
 # Test 
 
 # import some data to play with
-iris = datasets.load_iris()
+digit = datasets.load_breast_cancer()
 
 train_set = 0.7
 nb_classes = 3
-x = iris.data
-y = iris.target
+x = digit.data
+y = digit.target
 
 x_train = x[len(x)-int(len(x)*train_set):]
 y_train = y[len(y)-int(len(y)*train_set):]
@@ -55,4 +69,49 @@ y_train = y[len(y)-int(len(y)*train_set):]
 x_test = x[:len(x)-int(len(x)*train_set)]
 y_test = y[:len(y)-int(len(y)*train_set)]
 
-models = SVM_Enssemble(3,x_train,y_train,0.3)
+#models = SVM_Enssemble(20,x_train,y_train,0.2)
+#y_pred = prdict_test(x_test,models,2)
+
+
+
+
+def performance(y_test,y_pred):
+	cpt = 0
+	for i in range(len(y_test)):
+
+		if y_pred[i] == y_test[i]:
+			cpt = cpt +1
+
+	return (cpt/len(y_test))
+
+
+#Incremental cardinality 
+
+#ox  = []
+#oy = []
+#for i in range(2,20):
+#	models = SVM_Enssemble(i,x_train,y_train,0.3)
+#	ox.append(i)
+#	y_pred = prdict_test(x_test,models,2)
+#	oy.append(performance(y_test,y_pred))
+
+
+#Same number of SVMs (20)
+#ox  = []
+#oy = []
+#p = 0.1
+#while p<0.7:
+#	models = SVM_Enssemble(20,x_train,y_train,p)
+#	ox.append(p)
+#	y_pred = prdict_test(x_test,models,2)
+#	oy.append(performance(y_test,y_pred))
+#	p = p + 0.1
+
+
+
+
+
+#plt.plot(ox, oy)
+#plt.show()
+
+#https://careers.3ds.com/fr/jobs/stage-marketing-data-analyst-h-f-515755
